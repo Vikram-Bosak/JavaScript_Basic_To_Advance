44623326554e926a88318ee224de9bfcf1fb0573
vikramkumarbosak89@gmail.com <vikramkumarbosak69@gmail.com>
2022-08-31 23:00:17 +0530

    lete study started

--- string_matho.js
+++ string_matho.js
@@ -1,10 +1,26 @@
-// trim()
-// toUpperCase()
-// toLowerCase()
-// slice()
-
-let firstName = "   vikram     ";
+//== trim()
+let firstName = "   viKram     ";
 
 console.log(firstName.length);
 firstName = firstName.trim();
 console.log(firstName);
+
+//== toUpperCase()
+firstName=firstName.toUpperCase();
+console.log(firstName);
+
+
+
+//== toLowerCase()
+firstName=firstName.toLocaleLowerCase();
+console.log(firstName);
+
+
+
+//== slice()
+// start index
+// end index
+
+let newString = firstName.slice (0,4) // hars
+
+console.log(newString);
---
+++ typeof_operator.js
@@ -0,0 +1,27 @@
+// typeof operators
+
+
+// data type (primitive data types
+// strubg " vikram"
+// number 1, 2, 3 , 4
+// Boolean
+// undefined
+// null
+// BigInt
+// Symbol
+
+let age =25 ;
+let firstName = "vikram";
+
+console.log(typeof(age));
+console.log(typeof(firstName));
+
+// ==convert number to String
+
+console.log(typeof(age + ""));
+
+
+// ==convert string to number
+
+let myStr = + "25";
+console.log(typeof(myStr));
