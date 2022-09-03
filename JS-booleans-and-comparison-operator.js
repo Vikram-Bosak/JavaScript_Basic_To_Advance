572e078a3f79b311e1713f8ef1a9f2fce2eb3daf
vikramkumarbosak89@gmail.com <vikramkumarbosak69@gmail.com>
2022-09-03 06:11:06 +0530

    now if else program

--- JS-booleans-and-comparison-operator.js
+++ JS-booleans-and-comparison-operator.js
@@ -1,2 +1,20 @@
 // == Boolean & comparison operator
 
+
+
+
+// Boolean
+// true and  false
+
+let num1 ="5";
+let  num2 =5;
+
+console.log(num1>num2);
+
+//== 1. == vs ===
+
+console.log(num1===num2);
+
+//==  != vs !==
+
+console.log(num1 !== num2);
---
+++ truthy-and-falsy-values.js
@@ -0,0 +1,3 @@
+// truthy and falsu values
+
+
